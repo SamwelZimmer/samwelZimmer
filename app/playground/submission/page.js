d0d15348322b19cc2461870906b08ea10ee53f9b
Sam <sampharris2000@hotmail.co.uk>
2022-11-11 18:19:42 +0000

    window defined

--- app/playground/submission/page.js
+++ app/playground/submission/page.js
@@ -9,8 +9,10 @@ import { BsFillCaretLeftFill } from "react-icons/bs";
 import { AiFillHome } from "react-icons/ai";
 
 function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return { width, height};
+    if (typeof window !== 'undefined') {
+        const { innerWidth: width, innerHeight: height } = window;
+        return { width, height};
+    }
 }
 
 export default function PlaygoundPage() {
@@ -21,8 +23,8 @@ export default function PlaygoundPage() {
     const [valid, setValid] = useState(false);
     const [submitted, setSubmitted] = useState(false);
 
-    const percentX = (mousePosition.x / windowDimensions.width) * 100
-    const percentY = (mousePosition.y / windowDimensions.height) * 100
+    const percentX = (mousePosition.x / windowDimensions?.width) * 100
+    const percentY = (mousePosition.y / windowDimensions?.height) * 100
 
     const changeToSignUp = () => {
         setIsSignUp(!isSignUp)
@@ -30,7 +32,6 @@ export default function PlaygoundPage() {
 
     const emailValidation = () => {
         const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-        console.log(expression.test(String(email).toLowerCase()))
         return expression.test(String(email).toLowerCase())
     }
 
@@ -44,20 +45,24 @@ export default function PlaygoundPage() {
         setSubmitted(true);
     }
 
-    useEffect(() => {
-        const handleMouseMove = (e) => {
-            setMousePosition({ x: e.clientX, y: e.clientY });
-        };
+    if (typeof window !== 'undefined') {
+        useEffect(() => {
+            const handleMouseMove = (e) => {
+                setMousePosition({ x: e.clientX, y: e.clientY });
+            };
+    
+            window.addEventListener('mousemove', handleMouseMove);
+    
+            return () => {
+                window.removeEventListener(
+                  'mousemove',
+                  handleMouseMove
+                );
+            }; 
+        }, []);
+    }
 
-        window.addEventListener('mousemove', handleMouseMove);
 
-        return () => {
-            window.removeEventListener(
-              'mousemove',
-              handleMouseMove
-            );
-        }; 
-    }, []);
 
 
     const mouseVariants = {
