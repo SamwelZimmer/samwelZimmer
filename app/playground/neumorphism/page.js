3fdf3d0556d61340c718ec59a25634220344c257
Sam <sampharris2000@hotmail.co.uk>
2022-11-17 16:00:00 +0000

    neumorph nav buttons

--- app/playground/neumorphism/page.js
+++ app/playground/neumorphism/page.js
@@ -1,6 +1,9 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Link from "next/link";
+import { GrHomeOption } from "react-icons/gr";
+import { IoChevronBackOutline } from "react-icons/io5";
 
 import './styles.css';
 
@@ -14,7 +17,6 @@ export default function Neumorphism() {
 
     const pressButton = () => {
         setIsPressed(!isPressed)
-        console.log(isPressed)
     }
 
     const changeColor = (colorHex, shadowHex, highlightHex) => {
@@ -28,11 +30,44 @@ export default function Neumorphism() {
         console.log(selectedColor)
     }
 
+    function BackButton () {
+        const [backPressed, setBackPressed] = useState(false);
+        const backPress = () => {
+            setBackPressed(!backPress);
+        }
+        return (
+            <Link className='absolute top-[10%] left-[10%]' href="/playground">
+                <div className="w-full grid items-center justify-center">
+                    <button onClick={() => backPress()} className={`displayed-container${backPressed ? "-pressed" : ""}-${selectedColor} w-[70px] md:w-[60px] rounded-xl aspect-square flex justify-center items-center text-4xl md:text-base`}>
+                        <IoChevronBackOutline style={{ opacity: "0.5"}} />
+                    </button>
+                </div>
+            </Link>
+        )
+    }
 
-
+    function HomeButton() {
+        const [homePressed, setHomePressed] = useState(false);
+        const homePress = () => {
+            setHomePressed(!homePressed);
+        }
+        return (
+            <Link className='absolute top-[10%] right-[10%]' href="/">
+                <div className="w-full grid items-center justify-center">
+                    <button onClick={() => homePress()} className={`displayed-container${homePressed ? "-pressed" : ""}-${selectedColor} w-[70px] md:w-[60px] rounded-xl aspect-square flex justify-center items-center text-4xl md:text-base`}>
+                        <GrHomeOption style={{ opacity: "0.5"}} />
+                    </button>
+                </div>
+            </Link>
+        )
+    }
 
     return (
-      <main className={`bg-${selectedColor} h-screen flex flex-col pb-2`}>
+      <main className={`bg-${selectedColor} h-screen flex flex-col justify-center items-center pb-2`}>
+
+        <BackButton />
+        <HomeButton />
+
         <section className='w-full flex flex-col items-center justify-center py-32'>
             <div className='grid grid-flow-row grid-cols-4 md:grid-cols-8 w-2/3 gap-6'>
             <button className={`select-button${selectedColor === "white" ? "-pressed" : ""}-${selectedColor} w-[50px] aspect-square p-2`}>
@@ -66,22 +101,17 @@ export default function Neumorphism() {
 
         <section className='py-32'>
             <div className="w-full grid items-center justify-center">
-                <button onClick={() => pressButton()} className={`displayed-container displayed-container${isPressed ? "-pressed" : ""}-${selectedColor} w-[100px] aspect-square`}></button>
+                <button onClick={() => pressButton()} className={`displayed-container displayed-container${isPressed ? "-pressed" : ""}-${selectedColor} w-[125px] md:w-[100px] aspect-square`}></button>
             </div>
           
         </section>
-        <h3 className="absolute bottom-0 text-lg w-full text-center opacity-70 text-slate-300 px-6 pb-2">
+            <h3 className="absolute bottom-0 text-lg w-full text-center opacity-70 text-slate-300 px-6 pb-2">
                 An evolution on {"'"}skeuomorphic{"'"} design.
                 <br/>
                 Elements sit on the background, yet slightly raised, whilst using similar materials and textures.
             </h3>
-
-
-
-
-        
-        
       </main>
-   
+
+
     );
-  }
+}
--- app/playground/page.js
+++ app/playground/page.js
@@ -22,27 +22,27 @@ export default function PlaygoundPage() {
           <div className='grid w-full h-full grid-cols-2  md:grid-cols-3 lg:grid-col-4 xl:grid-col-6 gap-12'>
             <Link href="/playground/neumorphism" className={`w-full h-full text-5xl md:text-2xl px-2 py-8 flex flex-col rounded-xl displayed-container ${randomBoxShape()}`}>
               <h4 className='dark:text-white opacity-90'>neumorphism</h4>
-              <p className='dark:text-white opacity-50'>a design style</p>
+              <p className='dark:text-white text-4xl md:text-xl opacity-50'>a design style</p>
             </Link>
             <Link href="/playground/submission" className={`w-full h-full text-5xl md:text-2xl px-2 py-8 flex flex-col rounded-xl displayed-container ${randomBoxShape()}`}>
               <h4 className='dark:text-white opacity-90'>slippery submisson</h4>
-              <p className='dark:text-white opacity-50'>a glassmorphic form with a twist</p>
+              <p className='dark:text-white text-4xl md:text-xl opacity-50'>a glassmorphic form with a twist</p>
             </Link>
             <Link href="/playground/horse-hopping" className={`w-full h-full text-5xl md:text-2xl px-2 py-8 flex flex-col rounded-xl displayed-container ${randomBoxShape()}`}>
               <h4 className='dark:text-white opacity-90'>horse hopping</h4>
-              <p className='dark:text-white opacity-50'>an algo for chess knights</p>
+              <p className='dark:text-white text-4xl md:text-xl opacity-50'>an algo for chess knights</p>
             </Link>            
             <Link href="/playground/fen-generator" className={`w-full h-full text-5xl md:text-2xl px-2 py-8 flex flex-col rounded-xl displayed-container ${randomBoxShape()}`}>
               <h4 className='dark:text-white opacity-90'>fen gen</h4>
-              <p className='dark:text-white opacity-50'>a visual generator for chess layouts</p>
+              <p className='dark:text-white text-4xl md:text-xl opacity-50'>a visual generator for chess layouts</p>
             </Link>            
             <div className={`w-full h-full text-5xl md:text-2xl px-2 py-8 flex flex-col rounded-xl displayed-container ${randomBoxShape()}`}>
               <h4 className='dark:text-white opacity-90'>project 2</h4>
-              <p className='dark:text-white opacity-50'>project 2 sub</p>
+              <p className='dark:text-white text-4xl md:text-xl opacity-50'>project 2 sub</p>
             </div>            
             <div className={`w-full h-full text-5xl md:text-2xl px-2 py-8 flex flex-col rounded-xl displayed-container ${randomBoxShape()}`}>
               <h4 className='dark:text-white opacity-90'>project 2</h4>
-              <p className='dark:text-white opacity-50'>project 2 sub</p>
+              <p className='dark:text-white text-4xl md:text-xl opacity-50'>project 2 sub</p>
             </div>
           </div>
         </section>
